06698784b8889365062a8b8f93e7d431183e1887
Robin <robinwuerz@web.de>
2018-04-05 14:19:36 +0200

    Update 20180312_dump_coms_diff_structure_after_v1.sql

--- mysql_dump/20180312_dump_coms_diff_structure_after_v1.sql
+++ mysql_dump/20180312_dump_coms_diff_structure_after_v1.sql
@@ -1,6 +1,20 @@
 -- New create with expirtation date: create_cert_part is now depricated
 
-DELIMITER $$
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `create_cert_part_2` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `create_cert_part_2`(IN PART_ID CHAR(255), IN CERT_ID CHAR(255))
 BEGIN
 
@@ -21,5 +35,4 @@ UPDATE `coms_certificate_participant` set coms_certificate_participant_3digit = 
 
 UPDATE `coms_certificate_participant` set coms_certificate_participant_id_base32 = LPAD(CONV(concat(coms_certificate_participant_id,coms_certificate_participant_3digit),10,32),8,'0') where coms_certificate_participant_id = @coms_certificate_participant_ID;
 
-END$$
-DELIMITER ;
+END ;;
