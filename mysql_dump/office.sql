c4f4aa23f9a75799763e297c96039ec97ab95ddd
Robin <robinwuerz@web.de>
2018-04-20 14:28:58 +0200

    Create office.sql
    These are the phpmyadmin rights for the current office.

---
+++ mysql_dump/office.sql
@@ -0,0 +1,16 @@
+-- Rights for the office phpmyadmin account: Replace 'email' with the respective email address for the current office.
+
+GRANT SELECT, UPDATE ON `bpmspace_coms_v1`.* TO email;
+
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`add_part_event` TO email;
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`create_trainer` TO email;
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`add_trainer_ato` TO email;
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`add_trainer_exam` TO email;
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`add_trainerorg_exam` TO email;
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`create_proctor` TO email;
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`add_proctor_ato` TO email;
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`create_part_event` TO email;
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`create_event` TO email;
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`create_cert_part` TO email;
+GRANT EXECUTE ON PROCEDURE `bpmspace_coms_v1`.`create_cert_part_2` TO email;
+FLUSH PRIVILEGES;
