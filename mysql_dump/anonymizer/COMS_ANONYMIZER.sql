6ec20cf7ba6ad69a20511d5d5d1d193451121b5d
Robin <robinwuerz@web.de>
2018-06-13 14:47:01 +0200

    Update COMS_ANONYMIZER.sql

--- mysql_dump/anonymizer/COMS_ANONYMIZER.sql
+++ mysql_dump/anonymizer/COMS_ANONYMIZER.sql
@@ -386,15 +386,6 @@ SET   moodle.mdl_offlinequiz.name = coms_exam_event.coms_exam_event_name;
 
 -- Anonymize Moodle names	
 											    
-Update
-
-  moodle.mdl_offlinequiz
-
-JOIN coms_exam_event on moodle.mdl_offlinequiz.name like concat( '%',coms_exam_event.coms_exam_event_id_base32,'%' )
-
-SET   moodle.mdl_offlinequiz.name = coms_exam_event.coms_exam_event_name;
-
--- anonymize users
 Update `moodle`.`mdl_user`
 
 JOIN `bpmspace_coms_v1_A`.`coms_participant` on LEFT(moodle.mdl_user.idnumber,length(moodle.mdl_user.idnumber)-3) = coms_participant_id
