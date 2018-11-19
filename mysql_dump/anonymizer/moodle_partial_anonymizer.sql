4ed5971f993bc4a78b8c5d98bfdaf31c41e29688
rootmessages BPMspace <rootmessages@bpmspace.com>
2018-11-19 08:30:04 +0100

    Update moodle_partial_anonymizer.sql

--- mysql_dump/anonymizer/moodle_partial_anonymizer.sql
+++ mysql_dump/anonymizer/moodle_partial_anonymizer.sql
@@ -1,16 +1,18 @@
 -- anonymize names
+SET SQL_SAFE_UPDATES = 0;
+
 Update
 
   moodle.mdl_offlinequiz
 
-JOIN coms_exam_event on moodle.mdl_offlinequiz.name like concat( '%',coms_exam_event.coms_exam_event_id_base32,'%' )
+JOIN `bpmspace_coms_v2_TEST`.`coms_exam_event` on moodle.mdl_offlinequiz.name like concat( '%',bpmspace_coms_v2_TEST.coms_exam_event.coms_exam_event_id_base32,'%' )
 
-SET   moodle.mdl_offlinequiz.name = coms_exam_event.coms_exam_event_name;
+SET   moodle.mdl_offlinequiz.name = `bpmspace_coms_v2_TEST`.coms_exam_event.coms_exam_event_name;
 
 -- anonymize users
 Update `moodle`.`mdl_user`
 
-JOIN `bpmspace_coms_v1_A`.`coms_participant` on LEFT(moodle.mdl_user.idnumber,length(moodle.mdl_user.idnumber)-3) = coms_participant_id
+JOIN `bpmspace_coms_v2_TEST`.`coms_participant` on LEFT(moodle.mdl_user.idnumber,length(moodle.mdl_user.idnumber)-3) = coms_participant_id
 
 SET   moodle.mdl_user.lastname = coms_participant.coms_participant_lastname, moodle.mdl_user.firstname = coms_participant.coms_participant_firstname;
 
